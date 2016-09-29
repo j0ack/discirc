62b47a5b7ef61473b94744039bde085f66ee67fc
joachim <jtrouverie@itrust.fr>
2016-09-29 11:56:06 +0200

    setup.py

--- README.rst
+++ README.rst
@@ -1,4 +1,30 @@
 Torii
 =====
 
-A discord to irc gateway
+**Torii** is a bot that syncs messages between `Discord <https://discordapp.com/>`_ and `IRC <http://www.irc.org/>`_. It's written in Python using the `discord.py <https://github.com/Rapptz/discord.py>`_ and `bottom <https://github.com/numberoverzero/bottom>`_ libraries.
+
+Installation
+------------
+
+You can install it simply using `pip`::
+
+  $ pip install torii
+
+Use
+---
+
+Launch
+******
+
+You first need to configure torii thanks to a config file. By default **Torii** tries to load a `~/.toriirc` file, if not present you need to give it to the app with the `-- config` option.
+
+To create a valid config file please see the `example <https://github.com/j0ack/blob/master/config-example.json>`_.
+
+Then simply run **Torii** thanks to the available command::
+
+  $ torii
+
+Private messages
+****************
+
+**Torii** handles private messages. You need to prefix your message with a `@User` when sent from **Discord** and with `User:` from IRC. 
---
+++ setup.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+__author__ = 'TROUVERIE Joachim'
+__version__ = '0.1'
+__appname__ = 'Torii'
+__email__ = 'joachim.trouverie@linoame.fr'
+
+
+requirements = []
+for line in open('REQUIREMENTS.txt', 'r'):
+    requirements.append(line)
+
+setup(
+    name=__appname__,
+    version=__version__,
+    packages=find_packages(),
+    author=__author__,
+    author_email=__email__,
+    description='A Discord to IRC gateway', 
+    long_description=open('README.rst').read(),
+    install_requires=requirements,
+    include_package_data=True,
+    url='http://pythonhosted.org/torii/',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Topic :: Communications :: Chat",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",        
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+    ],
+    entry_points={
+        'console_scripts': [
+            'torii = torii.launcher:main',
+        ],
+    },
+)
+
