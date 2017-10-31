1ca16d69a360ade1b971d437593a23ef5e71d5f5
tamas <tmarki@gmail.com>
2017-10-31 10:35:35 +0100

    MOTD function decription changed

--- discirc/irc_bot.py
+++ discirc/irc_bot.py
@@ -73,10 +73,8 @@ class IRCBot(bottom.Client):
         if self.password:
             self.send('PASS', password=self.password)
 
+     """checking if Message Of The Day is done because it may interfere with joining channels"""
     def on_motddone(self, message):
-        """On MOTD received or no MOTD available event. MOTD stands for Message Of The Day, as per RFC 1459.
-        Clients should wait with issuing JOIN commands until either of these
-        messages are received."""
         for chan in self.channels.values():
             if chan not in self.channelPass:
                 self.send('JOIN', channel=chan)
